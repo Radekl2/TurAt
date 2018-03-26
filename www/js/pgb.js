7efc01e49756196889bd560ed9b3406ae655b35a
Radoslaw Smyksy <rsmyksy@gmail.com>
2018-03-26 17:59:45 +0200

    bug fixes

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -5,8 +5,4 @@ function init() {
 function onDeviceReady() {
 }
 
-function openPopupWithSize(){
-    searchPic = new Image(100,100);
-    searchPic.src = "../img/pom.png";
-	$("#pic").attr("src", searchPic.src);
-}
+
